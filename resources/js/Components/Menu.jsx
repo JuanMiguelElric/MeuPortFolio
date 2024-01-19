9608fc88c69c464223bcf8ce113e25cc8d39c237
JuanMiguelElric <oliveirajuan243@gmail.com>
2024-01-19 09:20:54 -0300

    Criação da pagina de agradecimento finalização da autenticação

--- resources/js/Components/Menu.jsx
+++ resources/js/Components/Menu.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import logo from "../../../public/images/logo.png"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../css/app.css'
-
+import { Link } from '@inertiajs/react';
 const Menu = () =>{
     console.log('menu');
     return(
@@ -13,7 +13,11 @@ const Menu = () =>{
                         <img src={logo} style={{width:'6em'}} alt="" />
                     </div>
                     <ul className="d-flex p-3 ">
-                        <li className="d-none d-xl-block">Home</li>
+                        <li className="d-none d-xl-block">
+                            <Link className="link" href="/">
+                                Home
+                            </Link>
+                        </li>
                         <li className="d-none d-xl-block">Sobre mim</li>
                         <li className="d-none d-xl-block">Habilidades</li>
                         <li className="d-none d-xl-block">Projetos</li>
--- resources/js/Pages/Bemsucedido.jsx
+++ resources/js/Pages/Bemsucedido.jsx
@@ -1,16 +1,36 @@
 import { Head } from "@inertiajs/react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../css/app.css';
+import { MdVerified } from "react-icons/md";
 import React from "react";
-
+import Menu from "@/Components/Menu";
+import { Zoom } from "react-reveal";
+import { Link } from '@inertiajs/react';
 const Bemsucedido = () =>{
     return(
         <>
         <Head title="Bemsucedido" />
-            <div className="container-fluid">
+            <div className="container-fluid backgroundimage">
+                <Menu />
                 <div className="row">
-                    <div className="col-sm-12">
-                        <div className="text-center">aaaaaaaaaa</div>
+                    <div className="col-sm-12 p-5">
+                        <div className="text-center">
+                            <Zoom >
+                                <h1><MdVerified color="green" size={200} /></h1>
+                            </Zoom>
+                            <h1 style={{fontSize:'4em',color:"green"}}>Dados enviados com sucesso!</h1>
+                            <br />
+                            <br />
+                            <p style={{fontSize:'2em', color:"#fffff"}}>Em breve, entrarei em contato com você. Enquanto aguarda, que tal desfrutar <br />de uma pausa com um gole de café e explorar o meu site?</p>
+                            <br />
+                            <br />
+                            <button className="btn btn-success">
+                                <Link href="/">
+                                    Clique aqui!
+                                
+                                </Link>
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
